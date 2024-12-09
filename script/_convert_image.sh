26fbb92c07e3a25f9ef0009a359695830ac9fe2a
TomoTom0 <tomoIris427+GitHub@gmail.com>
2024-12-09 16:00:45 +0900

    Refactor drawio file conversion in _convert_image.sh script

--- script/_convert_image.sh
+++ script/_convert_image.sh
@@ -17,12 +17,30 @@
   echo "Converting drawio files to svg..."
   while read filename; do
     filename_main=$(echo $filename | rev | sed -e "s/.*oiward\.//" | rev)
-    if [[ ! img/${filename_main}_notext.svg -nt img/${filename} ]]; then
-      drawio -xrf pdf --crop -o tmp/ img/${filename}
-      pdftocairo -svg tmp/${filename_main}.pdf img/${filename_main}_notext.svg
-    else
-      echo "skipped: ${filename} is not updated"
-    fi
+    # if [[ ! img/${filename_main}_notext.svg -nt img/${filename} ]]; then
+    echo "Converting ${filename}"
+    xml_name="tmp.xml"
+    drawio -xf xml -o tmp/$xml_name img/${filename}
+    count=0
+    page_count=$(grep -P "^\s*<diagram" tmp/${xml_name} | wc -l)
+    echo "page_count: ${page_count}"
+    while read -r page_name; do
+      echo "$page_name"
+      export_name="img/${filename_main}_${page_name}_notext.svg"
+      if ((page_count == 1)); then
+        export_name="img/${filename_main}_notext.svg"
+      fi
+      if [[ ${export_name} -nt img/${filename} ]]; then
+        echo "skipped: ${export_name} is not updated"
+        continue
+      fi
+      drawio -xf pdf -p $count --crop -o tmp/tmp.pdf img/${filename}
+      pdftocairo -svg tmp/tmp.pdf $export_name
+      ((count++))
+    done < <(grep -P "^\s*<diagram" tmp/${xml_name} | grep -oP '(?<=name=")[^"]*(?=")')
+    # drawio -xrf pdf --crop -o tmp/ img/${filename}
+    # pdftocairo -svg tmp/${filename_main}.pdf img/${filename_main}_notext.svg
+    # fi
   done < <(find img -name "*.drawio*" -printf "%f\n")
   rm -rf tmp/*
 
@@ -36,7 +54,7 @@
       echo "skipped: ${filename_main}.svg is not updated"
     fi
   done < <(find img -name "*.svg" -not -name "*_notext.svg" -printf "%f\n" |
-   rev | sed s/gvs.// | rev)
+    rev | sed s/gvs.// | rev)
   rm -rf tmp/*
 
   echo ""
